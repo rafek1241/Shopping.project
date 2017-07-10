ae785f024fe42891b416234665cd64136f608dd7
Robert Szachnowski <rszachnowski@wi.ps.pl>
2017-07-10 21:23:48 +0200

    RS -  Initial Settings. Core prepared for first Area.

---
+++ Shopping.Core/Shopping.Core/Areas/ShopPage/ShopPageAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Shopping.Core.Areas.ShopPage
+{
+    public class ShopPageAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ShopPage";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ShopPage_default",
+                "ShopPage/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Shopping.Core/Shopping.Core/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Shopping.Core.MvcApplication" Language="C#" %>
